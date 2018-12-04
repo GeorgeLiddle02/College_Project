dc329b0cc95e8b22234263d31d601b39c63080c0
SUSSEXDOWNS\479995 <479995@chrome.sussexdowns.ac.uk>
2018-12-04 10:58:06 +0000

    progress on the muzzle flash and enemy can now chase the player.

---
+++ College Unity Project/Assets/Scripts/fireEffects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireEffects : MonoBehaviour {
+    public ParticleSystem _muzzleFlash;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

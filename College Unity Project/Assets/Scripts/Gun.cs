0e341648fa58d41508ab8ac612c39ffddf0b8d33
SUSSEXDOWNS\479995 <479995@chrome.sussexdowns.ac.uk>
2018-12-06 14:41:09 +0000

    Added muzzle flash AGAIN but it won't follow the gun.

--- College Unity Project/Assets/Scripts/Gun.cs
+++ College Unity Project/Assets/Scripts/Gun.cs
@@ -4,18 +4,25 @@ public class Gun : MonoBehaviour {
 
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 10f;
 
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
     void Shoot()
         {
+
+        muzzleFlash.Play();
 
             RaycastHit hit;
             if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
